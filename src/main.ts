91c04eb4c1726a3512738a580ac7aaec94b0cfb3
Steve Belovarich <steveblue@gmail.com>
2018-12-09 20:47:24 -0800

    cleanup main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import { ComponentMeta, html, css } from './decorators/component';
 import { RedButtonComponent } from './lib/components/button/red-button.component';
 import { BlueButtonComponent } from './lib/components/button/blue-button.component';
-
 
 customElements.define('red-button', RedButtonComponent, { extends: 'button'});
 customElements.define('blue-button', BlueButtonComponent, { extends: 'button'});
