ad9903590d1df00a87156e3dc71061533cec9dd1
Steve Belovarich <steveblue@gmail.com>
2019-01-01 13:23:08 -0800

    deprecate local rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from 'rollup-plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
-import inlinePostCSS from './rollup.cssnano-plugin';
+import inlinePostCSS from 'rollup-plugin-inline-postcss';
 
 export default [
     {
--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -2,7 +2,7 @@ import typescript from 'rollup-plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
-import inlinePostCSS from './rollup.cssnano-plugin';
+import inlinePostCSS from 'rollup-plugin-inline-postcss';
 
 export default [
     {
